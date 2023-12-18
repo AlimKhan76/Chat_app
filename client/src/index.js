f490cf5847142109804c68189e158151f588cf89
Alim <coder34521@gmail.com>
2023-12-18 18:31:26 +0530

    Url of vercel server

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://chat-app-ark2-alimkhan76s-projects.vercel.app";
+axios.defaults.baseURL = "https://chat-app-ruby-gamma-89.vercel.app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
